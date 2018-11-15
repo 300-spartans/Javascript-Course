313d02d5361a3aa0e238564939436815f5613cbf
Sam <swright@tmcdesign.com>
2018-11-15 08:49:35 -0700

    Lesson 1

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,34 @@
+/*************************
+* Variables and data types
+*/
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
+/
+
+
+/***********************
+* Variable mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' is ' + age);
