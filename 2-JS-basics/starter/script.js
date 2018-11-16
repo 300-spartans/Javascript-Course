0a327dee8eda8e2605a0b8fcaddc2a01fe9aa70d
Sam <swright@tmcdesign.com>
2018-11-16 12:08:21 -0700

    Lecture 3

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -26,7 +26,7 @@ var if = 23;
 
 /***********************
 * Variable mutation and type coercion
-*/
+
 
 var firstName = 'John';
 var age = 28;
@@ -48,3 +48,75 @@ alert(firstName + 'is a ' + age + ' year old ' + job + '. Is he married? ' + isM
 
 var lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
+
+
+
+
+
+/**********************************
+* Basic Operators
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark= 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
+
+
+
+
+/********************************
+* Operator precedence
+*/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6;  // 8 * 4 - 6 // 32 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+
+// More Operators
+// x = x * 2;
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x ++;
+console.log(x);
