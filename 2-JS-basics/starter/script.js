9e09fddf49f3e93b1079e7adcedfacfe4b0e716c
Sam <swright@tmcdesign.com>
2018-11-20 09:33:21 -0700

    Coding Challenge 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -90,7 +90,7 @@ console.log(typeof x);
 
 /********************************
 * Operator precedence
-*/
+
 
 var now = 2018;
 var yearJohn = 1989;
@@ -120,3 +120,67 @@ x += 10;
 console.log(x);
 x ++;
 console.log(x);
+*/
+
+
+/*********************************
+* Coding Challenge
+
+
+var massMark = prompt("What is Mark's weight in kg?");
+var massJohn = prompt("What is John's weight in kg?");
+var heightMark = prompt("What is Mark's height in m?");
+var heightJohn = prompt("What is John's height in m?");
+
+var markBMI = massMark / (heightMark * heightMark);
+var johnBMI = massJohn > (heightJohn * heightJohn);
+
+var isMarkhigher = markBMI > johnBMI;
+
+console.log("Is Mark's BMI higher than John's? " + isMarkhigher);
+
+
+*/
+
+/*********************************************
+* If / else statements
+*/
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+
+
+
+
+var massMark = 78;    // kg
+var heightMark = 1.69;  // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log('Mark\'s BMI is higher than John\'s.');
+} else {
+  console.log('John\'s BMI is higher than Mark\'s.');
+}
+
+// var markHigherBMI = BMIMark > BMIJohn;
+//console.log('Is Mark\s BMI higher than John\s? ' + markHigherBMI);
